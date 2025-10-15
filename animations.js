b25a3b33727d290b613480cd7f278e9160065ac7
maxanlam <154699444+maxanlam@users.noreply.github.com>
2025-10-16 01:22:07 +0200

    Update animations.js

--- animations.js
+++ animations.js
@@ -49,38 +49,17 @@ function scrollTitle() {
 
 scrollTitle();
 
+const map = document.querySelector("#ascii-map");
+const text = map.textContent;
+const textArray = [...text];
+const indeces = textArray
+  .map((char, index) => ({ char, index }))
+  .filter((c) => c.char === "~" || c.char === "°");
 
-  const map = document.querySelector('#ascii-map')
-  const text = map.textContent
-  const textArray = [...text]
-  const indeces = textArray.map((char, index) => ({ char, index})).filter(c => c.char === '~' || c.char === '°')
-  
-  function animateMap() {
-    const randomIndeces = indeces.filter(c => Math.random() * 50 < Math.random())
-
-    const bubbles = textArray.map((char, index) => ({ char, index})).filter(c => c.char === 'o' && indeces.findIndex(char => char.index === c.index) >= 0)
-    const expandedBubbles = textArray.map((char, index) => ({ char, index})).filter(c => c.char === ':')
-    const poppedBubbles = textArray.map((char, index) => ({ char, index})).filter(c => c.char === '.')
-    const droplets = textArray.map((char, index) => ({ char, index})).filter(c => c.char === '°')
-    for (const c of randomIndeces) {
-      textArray[c.index] = 'o'
-    }
-    for (const bubble of bubbles) {
-      textArray[bubble.index] = ':'
-    }
-    for (const bubble of expandedBubbles) {
-      textArray[bubble.index] = '.'
-    }
-    for (const pBubble of poppedBubbles) {
-      textArray[pBubble.index] = '°'
-    }
-    for (const droplet of droplets) {
-      textArray[droplet.index] = '~'
-    }
-    map.textContent = textArray.join('')
-    setTimeout(animateMap, 150)
-  }
-
+function animateMap() {
+  const randomIndeces = indeces.filter(
+    (c) => Math.random() * 22 < Math.random()
+  );
 
   const bubbles = textArray
     .map((char, index) => ({ char, index }))
